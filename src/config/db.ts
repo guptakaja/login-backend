21bdc9484b709147938770e156979d825477a270
kajal <kajal990199@gmail.com>
2025-12-07 15:45:05 +0530

    Configured Render PostgreSQL connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,28 @@
+// import { Sequelize } from "sequelize";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// export const sequelize = new Sequelize(
+//   process.env.DB_NAME as string,
+//   process.env.DB_USER as string,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//   }
+// );
+
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-  }
-);
+export const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  logging: false
+});
